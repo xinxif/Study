1abb975877de06b38bbf20a177012b76870b9282
Shen <howeverfate@gmail.com>
2024-10-30 11:54:27 +0800

    BracketMatch has been copleted

--- Date Structure/BracketMatch.c
+++ Date Structure/BracketMatch.c
@@ -1,24 +1,18 @@
 #include"BracketMatch.h"
 
-static bool matched(const char r_bkt, const char l_bkt)
+static bool matched(const char r_bkt, const char l_bkt,stack *stk)
 {
-	bool success;
+	bool success = false;
 	//两个括号的ASCII码相加
 	unsigned char sum = r_bkt + l_bkt;
 	//结果等于两个匹配的括号的值，匹配成功
 	switch (sum)
 	{
-	case l_parentheses + r_parentheses:
-		success = true;
-		break;
+	case l_parentheses + r_parentheses:;
 	case l_square_brackets + r_square_brackets:
-		success = true;
-		break;
 	case l_curly_braces + r_curly_braces:
+		pop(*stk);//匹配成功弹出栈顶元素
 		success = true;
-		break;
-	default:
-		success = false;
 		break;
 	}
 	return success;
@@ -44,30 +38,24 @@ size_t bracket_match(const char* brackets, size_t lenght)
 		case r_parentheses:
 		case r_square_brackets:
 		case r_curly_braces:
-			top_chr = *top(stk);
-			if (matched(top_chr, bkt))
-			{
-				pop(stk);//匹配成功弹出栈顶元素
-			}
-			else
-			{
-				loop = false;
-			}
+			top_chr = top(stk) ? *top(stk) : 0X0;
+			loop = matched(top_chr, bkt, &stk);
 			break;
 		default:
 			break;
 		}
 	}
-	//printf("%c", *top(stk));
+#ifndef __BRACKET_MATCH_NDEBUG__
 	if (empty(stk))
 	{
 		puts("匹配成功");
-		i = 1u;
 	}
 	else
 	{
-		puts("失败");
+		puts("匹配失败");
 	}
+#endif // !__BRACKET_MATCH_NDEBUG__
+
 	destory_stack(stk);
-	return --i;
+	return loop ? 0U : i;//括号相对应返回实际下标+1的位置
 }
